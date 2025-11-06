07ca3d20556f8b13a19ef3c23ac6ccead0602bc9
mahirberkano <mahirberkano@gmail.com>
2025-11-06 18:13:04 +0300

    minor change...

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+# Install AWS CLI if not available
+if ! command -v aws >/dev/null 2>&1; then
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+  unzip awscliv2.zip
+  sudo ./aws/install
+  rm -rf awscliv2.zip aws/
+fi
+
 # Ensure Docker is available (if not preinstalled on AMI)
 if ! command -v docker >/dev/null 2>&1; then
   curl -fsSL https://get.docker.com | sh
